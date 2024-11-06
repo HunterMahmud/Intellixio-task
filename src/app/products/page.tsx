fd2f31945ae82f371c779bd0c10315084d6f2ffa
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-06 11:24:38 +0600

    Fix: Implemented challenges

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,6 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Products } from "@/views/products";
 
-
-
 export default function ProductsPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Products />
-    </Suspense>
-  );
+  return <Products />;
 }
