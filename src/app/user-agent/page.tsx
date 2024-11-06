048f7ed1857fe6383d2b89dcd3e4e51b957526c4
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-06 10:44:52 +0600

    Fix: Implemented challenges~

--- src/app/user-agent/page.tsx
+++ src/app/user-agent/page.tsx
@@ -1,7 +1,16 @@
-import { UserAgent } from "@/views/userAgent";
+// app/user-agent/page.tsx
+import { headers } from "next/headers";
+import { Providers } from "@/components/providers";
+import { UserAgent } from "@/views/userAgent"; 
 
 const UserAgentRoot = () => {
-  return <UserAgent />;
+  const userAgent = headers().get("user-agent") || "Unknown user agent";
+
+  return (
+    <Providers userAgent={userAgent}>
+      <UserAgent />
+    </Providers>
+  );
 };
 
 export default UserAgentRoot;
--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -8,8 +8,12 @@ import { ProductList } from "@/views/products/productList/productList";
 import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
 import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { useRouter, useSearchParams } from "next/navigation";
+
 
 export const Products: React.FC = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const {
     currentPage,
@@ -18,13 +22,40 @@ export const Products: React.FC = () => {
     handlePageChange,
   } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
+   
+  // Open modal based on URL parameter
+  useEffect(() => {
+    const productId = searchParams.get("product");
+    
+    if (productId) {
+      const product = PRODUCTS_DATA.find((p) => p.id === productId);
+      if (product) setSelectedProduct(product);
+    }
+  }, [searchParams]);
+
+  // Close modal if route changes away from products page
+  useEffect(() => {
+    // console.log("pathname: ", pathname);
+    // console.log("window location: ",window.location.href);
+    // const path = window.location.href;
+    const productId = searchParams.get("product");
+    // console.log("product id: ",productId);
+    
+    if (productId === null) {
+      setSelectedProduct(null);
+    }
+  }, [router, searchParams]);
+
+
   const handleOpenModal = useCallback((product: Product) => {
     setSelectedProduct(product);
-  }, []);
+    router.push(`?product=${product.id}`);
+  }, [router]);
 
   const handleCloseModal = useCallback(() => {
     setSelectedProduct(null);
-  }, []);
+    router.push("");
+  }, [router]);
 
   return (
     <div>
